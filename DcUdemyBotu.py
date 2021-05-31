0f3f8916a2ba974a628bcc03a4886671ccace1aa
Tr0gresS <84869439+Tr0gresS@users.noreply.github.com>
2021-05-31 15:48:11 +0300

    Add files via upload

---
+++ DcUdemyBotu.py
@@ -0,0 +1,48 @@
+from discord.ext import commands
+import requests, re
+from bs4 import BeautifulSoup
+
+
+def udemy():
+    response = requests.get("https://www.discudemy.com/feed/")
+    if(response.status_code == 200):
+        response_content = BeautifulSoup(response.content,"lxml")
+        item =  response_content.select("channel > item > description >a")
+     
+        for j in item:
+            url_code = j.get("href")
+            
+            first_index = url_code[26:]
+            p = re.search("/",first_index)
+            url_content = first_index[p.end():]
+            response_coupon = requests.get(f"https://www.discudemy.com/go/{url_content}")
+            
+            response_coupon_content = BeautifulSoup(response_coupon.content,"lxml")
+            url_coupon = re.search('<a href="(https://www.udemy.com/course.*) target=',str(response_coupon_content))
+            content = url_coupon.group(1)
+           
+            return content
+    else:
+        print(response.status_code)
+
+           
+bot = commands.Bot('-')
+
+
+if __name__ == "__main__":
+    
+    
+    @bot.command()
+    async def start(msg):
+        temp = None    
+        while True:
+            last = udemy()
+            if last != temp:
+                temp = last
+                print(temp)
+                await msg.send(temp)
+    
+      
+
+
+    bot.run("ODQ3NDMzMzE0NDEwMTY4MzIw.YK9_xg.oTrzyx0AVYaiUnZcpDex9-kNENY")
